67cc1890dc696aec05015085d0c16a53c7774146
sourav <mailsourav014@gmail.com>
2025-01-26 22:38:41 +0530

    updated routes

--- server/Routes/admin.js
+++ server/Routes/admin.js
@@ -1,6 +1,9 @@
 const { Router } = require("express");
 const { Admin } = require("../db/index");
+const { Patient } = require("../db/index");
+const adminMiddleware = require("../middlewares/adminMiddleware");
 const jwt = require("jsonwebtoken");
+
 require("dotenv").config();
 const secret = process.env.JWT_SECRET;
 const router = Router();
@@ -44,5 +47,61 @@ router.post("/signin", async (req, res) => {
     });
   }
 });
+router.get("/patient/view", adminMiddleware, async (req, res) => {
+  const response = await Patient.find({});
+  res.json({
+    response,
+  });
+});
+router.post("patient/signup", adminMiddleware, async (req, res) => {
+  const name = req.body.name;
+  const diseases = req.body.diseases;
+  const allergies = req.body.allergies;
+  const roomNumber = req.body.roomNumber;
+  const floorNumber = req.body.floorNumber;
+  const bedNumber = req.body.bedNumber;
+  const age = req.body.age;
+  const gender = req.body.gender;
+  const contactNumber = req.body.contactNumber;
+  const emergencyContact = req.body.emergencyContact;
+  const newPatient = await Patient.create({
+    name,
+    diseases,
+    allergies,
+    roomNumber,
+    floorNumber,
+    bedNumber,
+    age,
+    gender,
+    contactNumber,
+    emergencyContact,
+  });
+  res.status(200).json({
+    newPatient,
+  });
+});
 
+router.put("/patient/:patientId", adminMiddleware, async (req, res) => {
+  const patientId = req.params.patientId;
+  const patient = await Patient.findById(patientId);
+  if (patient) {
+    const morning = req.body.morning;
+    const evening = req.body.evening;
+    const night = req.body.night;
+    const instructions = req.body.instructions;
+    patient.foodChart.morning = morning;
+    patient.foodChart.evening = evening;
+    patient.foodChart.night = night;
+    patient.foodChart.instructions = instructions;
+
+    await patient.save();
+    res.json({
+      msg: "Patient details updated successfully",
+    });
+  } else {
+    res.status(404).json({
+      msg: "Patient does not exist",
+    });
+  }
+});
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const adminRouter = require("./Routes/admin");
-const patientRouter = require("./Routes/patient");
+
 const express = require("express");
 
 const app = express();
 app.use(express.json());
 
-app.use("/patient", patientRouter);
 app.use("/admin", adminRouter);
 
 app.listen(3000, () => {
